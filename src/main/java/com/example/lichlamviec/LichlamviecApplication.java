e814278f6fad8c8e7a8783a4ad835827c4e73151
huynhnga <huynhngaa001@gmail.com>
2024-06-25 14:24:51 +0700

    push file

---
+++ src/main/java/com/example/lichlamviec/LichlamviecApplication.java
@@ -0,0 +1,13 @@
+package com.example.lichlamviec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LichlamviecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LichlamviecApplication.class, args);
+	}
+
+}
